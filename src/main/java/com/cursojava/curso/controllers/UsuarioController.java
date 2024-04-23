90d9ec55843c4edbf97ac8815a785a04347d5c53
unknown <65382224+LuisFernandoReyes@users.noreply.github.com>
2024-04-23 12:51:33 -0600

    Creating user in UsuarioConttoller and Usuario.java. It givis me an JSON file with User data and I watched until 1:22:00 in the video

--- src/main/java/com/cursojava/curso/controllers/UsuarioController.java
+++ src/main/java/com/cursojava/curso/controllers/UsuarioController.java
@@ -1,13 +1,19 @@
 package com.cursojava.curso.controllers;
 
+import com.cursojava.curso.models.Usuario;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class UsuarioController {
 
-    @RequestMapping(value = "prueba")
-    public String prueba(){
-        return "Hola Prueba desde UsuarioController";
+    @RequestMapping(value = "usuario")
+    public Usuario getUsuario(){
+        Usuario usuario = new Usuario();
+        usuario.setNombre("Victor");
+        usuario.setApellido("Reynoso");
+        usuario.setEmail("Veko@hotmail.com");
+        usuario.setTelefono("7584968278");
+        return usuario;
     }
 }
